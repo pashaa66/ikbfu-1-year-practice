d20fa841cdd16c3c86f8810cef111d01a9b9f5b4
pashaa66 <pasha10428@gmail.com>
2025-07-01 22:41:46 +0200

    Project structure created User and announcements models created

---
+++ data/announcements.py
@@ -0,0 +1,30 @@
+import sqlalchemy
+from sqlalchemy import orm
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+from .db_session import SqlAlchemyBase
+
+
+class Announcements(SqlAlchemyBase, SerializerMixin, UserMixin):
+    __tablename__ = 'announcements'
+
+    id = sqlalchemy.Column(sqlalchemy.
+                           Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
+    location = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    price = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    announcement_type = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    square = sqlalchemy.Column(sqlalchemy.Float, nullable=True)
+    visits = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    kitchen_square = sqlalchemy.Column(sqlalchemy.Float, nullable=True)
+    number_of_rooms = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    is_sell = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
+    floor = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    number_of_floors = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    year_of_construction = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    path_to_photo = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.
+                                ForeignKey('users.id'), nullable=True)
+    user = orm.relationship('User')
---
+++ data/users.py
@@ -0,0 +1,34 @@
+import datetime as dt
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+
+
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    phone_number = sqlalchemy.Column(sqlalchemy.
+                                     String, unique=True, nullable=True)
+    experience = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    deals = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    role = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    announcement = orm.relationship('Announcements', back_populates='user')
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=dt.datetime.date(dt.
+                                                              datetime.now()))
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
