61c8e0079dfd458a61ab77d288328ffa04613c0f
pashaa66 <pasha10428@gmail.com>
2025-07-05 23:47:42 +0200

    menu for registration of realtor and buyer

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = os.path.join(os.basedir,
+                                           "static", "img", "uploads")
 
 
 @app.route("/")
 def home():
     return "Hello, world!"
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register_menu.html", title="Регистрация")
 
 
 if __name__ == "__main__":
