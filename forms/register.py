8a9b94a11ea7ef8f5227f5e043445750b4ec289b
pashaa66 <pasha10428@gmail.com>
2025-07-07 16:59:07 +0200

    Age restriction added

--- forms/register.py
+++ forms/register.py
@@ -2,14 +2,19 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField, EmailField, StringField
 from wtforms import IntegerField, TelField
 from flask_wtf.file import FileField, FileAllowed
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, NumberRange
 from forms.custom_validators import check_phone
 
 
 class RegisterFormUser(FlaskForm):
     name = StringField("Имя пользователя", validators=[DataRequired()])
     surname = StringField("Фамилия пользователя", validators=[DataRequired()])
-    age = IntegerField("Возраст пользователя", validators=[DataRequired()])
+    age = IntegerField(
+        "Возраст пользователя",
+        validators=[DataRequired(),
+                    NumberRange(min=18,
+                                message="Возраст должен быть не меньше 18 лет")
+                    ])
     email = EmailField("Почта", validators=[DataRequired()])
     password = PasswordField("Пароль",
                              validators=[DataRequired(), Length(min=8)])
@@ -21,7 +26,12 @@ class RegisterFormUser(FlaskForm):
 class RegisterFormRealtor(FlaskForm):
     name = StringField("Имя риэлтора", validators=[DataRequired()])
     surname = StringField("Фамилия риэлтора", validators=[DataRequired()])
-    age = IntegerField("Возраст риэлтора", validators=[DataRequired()])
+    age = IntegerField(
+        "Возраст риэлтора",
+        validators=[DataRequired(),
+                    NumberRange(min=20,
+                                message="Возраст должен быть не меньше 20 лет")
+                    ])
     email = EmailField("Почта", validators=[DataRequired()])
     phone_number = TelField("Номер телефона", validators=[DataRequired(),
                                                           check_phone])
